706725a3b48676af2300000c1de1e50f6e68aac2
mrilyavasilev6-droid <mr.ilya.vasilev.6@gmail.com>
2026-03-22 08:21:23 +0500

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 ADMIN_IDS = list(map(int, os.getenv('ADMIN_IDS', '').split(',')))
-PROVIDER_TOKEN = os.getenv('PROVIDER_TOKEN')
+PROVIDER_TOKEN = os.getenv('PROVIDER_TOKEN')  # для ЮKassa
 
 # Сначала пробуем взять DATABASE_URL из окружения (для Render)
 DATABASE_URL = os.getenv('DATABASE_URL')
