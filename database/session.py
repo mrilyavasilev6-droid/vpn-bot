a5b68989fcde056ed36dddd95ff32fa0549f37cf
mrilyavasilev6-droid <mr.ilya.vasilev.6@gmail.com>
2026-03-22 08:15:00 +0500

    Update session.py

--- database/session.py
+++ database/session.py
@@ -1,6 +1,6 @@
+from config import DATABASE_URL
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-from config import DATABASE_URL
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
